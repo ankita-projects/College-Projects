a8bc8f4a6673a4a73324bea97a7ecaf8bde9d541
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-02-03 22:31:21 +0200

    update

--- JS/Functions/setinterval/script.js
+++ JS/Functions/setinterval/script.js
@@ -1,8 +1,13 @@
 let a=0;
-setInterval(Animation,1000);
+let intervalID
+intervalID = setInterval(Animation,1000);
 
 function Animation(){
     a =a+10;
     let target =document.getElementById("box");
     target.style.marginLeft= a +"px";
 }
+document.getElementById("click").addEventListener("click",()=>{
+    clearInterval(intervalID);
+   
+})
