a226fdb6defcf53d40aab8bf1199cd1ec0f76daa
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-02-03 10:53:39 +0200

    learnings

---
+++ HTML_CSS/portfolio/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const header = document.getElementById("header");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.style.backgroundColor ="#f1f1f1";
+    header.style.padding ="10px";
+  
+  } else {
+    header.style.background ="#transparent";
+    header.style.fontSize = "60px";
+
+  }
+  
+}
+
