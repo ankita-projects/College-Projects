8958aafab3ce2f9ac0d5fce3d5ed5b3b08a2f98d
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-03-08 23:01:12 +0200

    my portfolio assignment

--- HTML_CSS/MyPortfolio/script.js
+++ HTML_CSS/MyPortfolio/script.js
@@ -1,44 +1,43 @@
-let header = document.querySelector("#header");
-let logo = document.querySelector("#logo");
+let header = document.querySelector(".navbar");
+let buttonBackToTop = document.getElementById("backToTop");
+let buttonMobile = document.getElementById("mobileButton");
+let nav = document.querySelector("nav");
+let links = document.querySelectorAll("nav ul li a");
 
-const minHeader = () => {
+window.onscroll = function () {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
   if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
   ) {
-    if (window.matchMedia("(max-width: 576px)").matches) {
-      logo.style.fontSize = "1rem";
-      header.style.backgroundColor = "#0a101f";
-    } else if (window.matchMedia("(max-width: 768px)").matches) {
-      logo.style.fontSize = "1.5rem";
-      header.style.backgroundColor = "#0a101f";
-    } else {
-      header.style.backgroundColor = "#0a101f";
-    }
+    header.classList.add("bg");
+    buttonBackToTop.style.display = "block";
   } else {
-    if (window.matchMedia("(max-width: 576px)").matches) {
-      logo.style.fontSize = "2rem";
-      header.style.backgroundColor = "#0a101f";
-    } else if (window.matchMedia("(max-width: 768px)").matches) {
-      logo.style.fontSize = "3rem";
-      header.style.backgroundColor = "#0a101f";
-    } else {
-      header.style.backgroundColor = "#0a101f";
-    }
+    header.classList.remove("bg");
+    buttonBackToTop.style.display = "none";
   }
 };
-document.addEventListener("scroll", minHeader);
 
-let backToTop = document.querySelector(".back-to-top");
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
-const toTop = () => {
-  if (
-    document.body.scrollTop > 600 ||
-    document.documentElement.scrollTop > 600
-  ) {
-    backToTop.style.bottom = "20px";
+const mobMenu = () => {
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", mobMenu);
+  }
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+    document.body.style.overflow = "";
   } else {
-    backToTop.style.bottom = "-50px";
+    nav.classList.add("responsive");
+    document.body.style.overflow = "hidden";
   }
 };
-document.addEventListener("scroll", toTop);
+
+buttonBackToTop.addEventListener("click", getToTop);
+buttonMobile.addEventListener("click", mobMenu);
