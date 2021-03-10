da9c23968499263ec40233ef0b2add6a00779c34
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-03-10 22:24:18 +0200

    updating speedgame

--- JS/Functions/SpeedGame/script.js
+++ JS/Functions/SpeedGame/script.js
@@ -1,10 +1,68 @@
-let bulb1 = document.getElementById("bulb1");
-let bulb2 = document.getElementById("bulb2");
-let bulb3 = document.getElementById("bulb3");
-let bulb4 = document.getElementById("bulb4");
-let myVar = setInterval(myTimer, 1000);                //Tmer runing in every 1sec
-const myTimer = ()=> {                                 // function generating random 4 no. for four bulbs
-  var d = Math.floor(Math.random() * 5);
-  document.getElementById("bulb"+d).style.backgroundColor="red";   //background color of bulb changes 
-}
-document.getElementById("start").addEventListener("click", myTimer);
+let scoredisplay = document.querySelector("#score");
+let overlay = document.getElementById("gameResult");
+let gameover = document.getElementById("displayScore");
+let close = document.getElementById("close");
+
+let hitCount = 0;
+let missCount = 0;
+let intervalId;
+let birdCount =0;
+let audio = new Audio();
+
+const playAudio = (source, loop) => {                 //function to provide what audio file to play on start & end of game
+  audio.src = source;
+  audio.autoplay = true;
+  audio.loop = loop;
+};
+
+const startGame = () => {
+  playAudio("sounds/bonus.wav", true);               //playing audio wave file on start of game(true means looping)
+  document.getElementById("stop").style.display = "block";            // make stop button visible
+  document.getElementById("start").style.display = "none";           // hide start button
+  document.querySelectorAll(".bulb").forEach((element) => {        // adding eventlistner for each of the bulbs
+    element.addEventListener("click", checkBird);                                                        
+  }); 
+  
+   intervalId = setInterval(() => {                            //for keep changing the bulbs in every 1 sec
+    document.querySelectorAll(".bulb").forEach((element) => {       //hiding away all the birds
+      element.style.setProperty("content",""); ;
+    });
+    var d = Math.floor(Math.random() * 4) + 1;                   // get random number between 1 and 4;
+    document.getElementById("bulb" + d).style.setProperty("content","url(./image.png)"); 
+    birdCount++;
+    if(birdCount>hitCount+3){                        // After missed three highlighted bulbs game ends
+      endGame();
+    } 
+  }, 1000);
+};
+document.getElementById("stop").style.display = "none";
+document.getElementById("start").addEventListener("click", startGame);
+
+const checkBird = (event) => {                  //to determine source of the event(which button is clicked)
+  let source = event.target || event.srcElement;
+  console.log(source.style.content);
+  if (source.style.content == 'url("./image.png")') {
+    hitCount++;
+  } else {
+       endGame();                                    // If player clicked the wrong bulb, then game over
+  }
+  scoredisplay.textContent = `Your score is ${hitCount}`;
+  if (missCount > 2) {
+    endGame();
+  }
+};
+
+const endGame = () => {
+  playAudio("sounds/gameOver.wav", false);
+  document.getElementById("start").style.display = "block"; // Make start button visible
+  document.getElementById("stop").style.display = "none"; // Hide stop button
+  clearTimeout(intervalId); // Stop timeout
+  console.log("Game over");
+  overlay.style.visibility = "visible"; // Make overlay visible
+  gameover.textContent = `Your score is ${hitCount} & You missed ${missCount} `; //Show score
+};
+
+const reload = () => {
+  window.location.reload();                             // reload page
+};
+close.addEventListener("click", reload);            // adding event listener to close button
